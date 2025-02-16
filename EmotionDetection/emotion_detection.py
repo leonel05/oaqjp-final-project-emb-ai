923b2995d0ab587b96b3b997d43cc9059d71444d
Leonel <adjeleonel@gmail.com>
2025-02-16 14:25:46 -0500

    Task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,21 +6,36 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=input_json, headers=headers)
+    print(response)
     formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
 
-    max_score = 0
-    max_key = None
-    for k,v in emotions.items():
-        if v > max_score:
-            max_score = v
-            max_key = k
+    if response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
 
-    return {
-    'anger': emotions['anger'],
-    'disgust': emotions['disgust'],
-    'fear': emotions['fear'],
-    'joy': emotions['joy'],
-    'sadness': emotions['sadness'],
-    'dominant_emotion': max_key
-    }
+        max_score = 0
+        max_key = None
+        for k,v in emotions.items():
+            if v > max_score:
+                max_score = v
+                max_key = k
+
+        emotions = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': max_key
+        }
+    
+    elif response.status_code == 400:
+        emotions = {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+        }
+
+    return emotions
--- server.py
+++ server.py
@@ -6,15 +6,16 @@ app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def sent_detector():
     text_to_analyze = request.args.get('textToAnalyze')
-    print(text_to_analyze)
     response = emotion_detector(str(text_to_analyze))
-    print(response)
 
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    else:
+        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=6000)
